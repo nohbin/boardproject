337d705d5f856faa5d03e1b6999ad95ec97cb9e1
Nohbin <nohbinmusic@gmail.com>
2023-11-14 15:52:29 +0900

    #16 - 게시글 관련 View Test 작성
    어떤 경로로 View 를 열지 테스트 명세서 작성

---
+++ src/test/java/com/boardp/boardproject/controller/ArticleControllerTest.java
@@ -0,0 +1,65 @@
+package com.boardp.boardproject.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@DisplayName("View Controller Test - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final  MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    @DisplayName("[view][Get] - 게시글 리스트 (게시판) 페이지 - 정상호출")
+    public void givenNothing_andRequestArticlesView_thenReturnsArticlesView() throws Exception {
+
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Test
+    @DisplayName("[view][Get] - 게시글 상세페이지 - 정상호출")
+    public void givenNothing_andRequestArticleView_thenReturnsArticleView() throws Exception {
+
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @Test
+    @DisplayName("[view][Get] - 게시글 검색 전용페이지 - 정상호출")
+    public void givenNothing_andRequestArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @Test
+    @DisplayName("[view][Get] - 게시글 해쉬태그 전용페이지 - 정상호출")
+    public void givenNothing_andRequestArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+
+        mvc.perform(get("/articles/hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+
+
+
+}
